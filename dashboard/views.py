60bf8e5e6a08bb1f52d78b8d86495ca4a896ce83
zahra.bahrami <z.bahrami@danalytica.com>
2023-06-21 21:54:33 +0330

    comment view functionality

--- dashboard/views.py
+++ dashboard/views.py
@@ -8,11 +8,11 @@ class DashboardView(View):
     def get(self, request):
         context = {}
         data = median_rate_per_platform()
-        context['data'] = data 
-        context['median'] = {item['platform']: item['median_of_exch_rate'] for item in data}
-        context['average'] = {item['platform']: item['average_of_exch_rate'] for item in data}
-        context['swaps'] = {item['platform']: item['number_of_swaps'] for item in data}
-        context['swappers'] = {item['platform']: item['number_of_swappers'] for item in data}
+        # context['data'] = data 
+        # context['median'] = {item['platform']: item['median_of_exch_rate'] for item in data}
+        # context['average'] = {item['platform']: item['average_of_exch_rate'] for item in data}
+        # context['swaps'] = {item['platform']: item['number_of_swaps'] for item in data}
+        # context['swappers'] = {item['platform']: item['number_of_swappers'] for item in data}
 
         context['data_time'] = data_per_time()
 
