f66c68cff31cda3eae08b7e3f77d80f0ba6e5321
zahra.bahrami <z.bahrami@danalytica.com>
2023-06-21 17:22:09 +0330

    heatmap added

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
-from .services import median_rate_per_platform
+from .services import median_rate_per_platform, data_per_time
 # Create your views here.
 
 class DashboardView(View):
@@ -13,5 +13,7 @@ class DashboardView(View):
         context['average'] = {item['platform']: item['average_of_exch_rate'] for item in data}
         context['swaps'] = {item['platform']: item['number_of_swaps'] for item in data}
         context['swappers'] = {item['platform']: item['number_of_swappers'] for item in data}
-   
+
+        context['data_time'] = data_per_time()
+
         return render(request, 'dashboard.html', context=context)
