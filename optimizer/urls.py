71dc76550d206d013f31c2c594d4e62821d39617
emilie <z.bahrami@danalytica.com>
2023-07-26 15:44:03 +0330

    views updated

--- optimizer/urls.py
+++ optimizer/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-# from .views import find_path, SaveDataInJsonView
+from .views import find_path, SaveDataInJsonView
 
 urlpatterns = [
-#     path('find_path/', find_path, name='find_path'),
-# 	path('updatejson/', SaveDataInJsonView.as_view(), name='updatejson'),
+    path('find_path/', find_path, name='find_path'),
+	path('updatejson/', SaveDataInJsonView.as_view(), name='updatejson'),
 
 ]
